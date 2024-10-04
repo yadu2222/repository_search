7e43c35580219ffacf20faa2b93849daa7ac75ae
yadu2222 <ochaocha2004@gmail.com>
2024-10-04 09:28:11 +0900

    [add]ユニットテストのためにmockを用意

---
+++ test/mock/repository_provider.mocs.dart
@@ -0,0 +1,6 @@
+import 'package:mockito/annotations.dart';
+import 'package:repository_search/view/providers/repository_porivider.dart';
+
+// Mock生成のためのアノテーション
+@GenerateMocks([RepositoryProvider])
+void main() {}
